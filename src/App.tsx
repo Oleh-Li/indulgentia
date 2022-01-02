985b03630a076c34b7fd93d57c3d5f9a9972f6c9
Oleh-Li <Oleh.7ee@gmail.com>
2022-01-02 09:09:20 +0200

    init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import InputForm from './components/inputForm/InputForm';
+
+function App() {
+  return (
+    <div className="App">
+<h3>Indulgentia init</h3>
+<InputForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/inputForm/InputForm.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import styles from "./inputForm.module.css"
+
+const InputForm = () => {
+  return (
+    <form action="submit">
+      <input type="text" autoComplete="off" placeholder="input name" />
+    </form>
+  );
+};
+
+export default InputForm;
