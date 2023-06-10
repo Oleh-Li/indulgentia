401f9975d16db6b0d100cf3d0045484f971ad817
Mangenda <oleh.7ee@gmail.com>
2023-06-10 16:46:16 +0300

    replace fetch with axios

--- src/services/fetchPic.ts
+++ src/services/fetchPic.ts
@@ -1,10 +1,10 @@
+import axios from "axios"
+
 function fetchPic(value: string, url: any) {
   const REACT_APP_KEY = "15354044-5c6c2e030b5f90cfcf13d54e3";
-  fetch(
-    `https://pixabay.com/api/?q=${value}&page=1&key=${REACT_APP_KEY}&image_type=photo&orientation=horizontal&per_page=10`
-  )
-    .then((response) => response.json())
-    .then((data) => {
+  axios(`https://pixabay.com/api/?q=${value}&page=1&key=${REACT_APP_KEY}&image_type=photo&orientation=horizontal&per_page=10`)
+    // .then((response) => response.json()) don't need anymore if use axios
+    .then(({ data }) => {
       const randomNumber = Math.ceil(Math.random() * data.hits.length - 1);
       url(data.hits[randomNumber].webformatURL);
       return data.hits;
