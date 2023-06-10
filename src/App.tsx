140f1408c646eaa36116aa35615811dcc854a8ef
Mangenda <oleh.7ee@gmail.com>
2023-06-10 17:10:44 +0300

    added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+//react-router-dom here
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./components/pages/HomePage/HomePage";
 import AboutUsPage from "./components/pages/AboutUsPage/AboutUsPage";
