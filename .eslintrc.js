ee5df59fb337348e9875809ab84dba7c3987a9ed
Oleh-Li <Oleh.7ee@gmail.com>
2023-09-21 14:19:31 +0300

    fix eslint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: [
+		"plugin:react/recommended",
+		"xo"
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 12,
+		sourceType: "module"
+	},
+	plugins: [
+		"react",
+		"@typescript-eslint"
+	],
+	rules: {
+		"no-unused-vars": "off",
+		quotes: [
+			"error",
+			"double"
+		],
+		semi: [
+			"error",
+			"never"
+		]
+	}
+}
--- src/components/Letter/Letter.tsx
+++ src/components/Letter/Letter.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { useSelector } from "react-redux";
 import { IRootState } from "../../interfaces";
 import styles from "./letter.module.css"
